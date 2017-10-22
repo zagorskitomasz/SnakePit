3652f32b2103f05d48bee9945574acb5b62e7a92
Tomasz Zagórski <30413731+zagorskitomasz@users.noreply.github.com>
2017-10-22 16:02:21 +0200

    Add files via upload

---
+++ Snakes/html/src/com/zagorskidev/snakes/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.zagorskidev.snakes.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.zagorskidev.snakes.MainClass;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(640, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MainClass();
+        }
+}
